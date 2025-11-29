5d283356dacae462dc3fb6b311c32d25124846e0
Akarsh Singh <akarshsingh4171@gmail.com>
2025-11-29 15:10:44 +0530

    Time: 60 ms (45.06%) | Memory: 54.9 MB (39.35%) - LeetSync

---
+++ 2821-timeout-cancellation/timeout-cancellation.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    
+    const timerId = setTimeout(() => {
+        fn(...args);
+    }, t);
+    
+    return function cancelFn() {
+        clearTimeout(timerId);
+    };
+};
