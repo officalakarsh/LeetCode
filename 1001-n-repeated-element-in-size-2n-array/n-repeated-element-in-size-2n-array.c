21d3cc63999cb925f1c420d25e40da1990284e6e
Akarsh Singh <akarshsingh4171@gmail.com>
2026-01-02 09:17:48 +0530

    Time: 22 ms (6.67%) | Memory: 10.2 MB (43.33%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int repeatedNTimes(int* nums, int numsSize) {
+   
+    qsort(nums, numsSize, sizeof(int), cmp);
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return nums[i];
+        }
+    }
+    
+    return -1;
+}
+
