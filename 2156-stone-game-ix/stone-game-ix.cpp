df252e2beb4869f81b917a2371bb8fa79323b562
Akarsh Singh <akarshsingh4171@gmail.com>
2026-08-16 21:07:11 +0530

    Time: 0 ms (100.00%) | Memory: 131.2 MB (92.89%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        int c0 = cnt[0];
+        int c1 = cnt[1];
+        int c2 = cnt[2];
+
+        // Even number of stones divisible by 3
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        // Odd number of stones divisible by 3
+        return abs(c1 - c2) > 2;
+    }
+};
